b995246d3cad6982482c48c3d23a516ef25ce7a7
Lake <tgalvin@lakelandcollege.edu>
2026-03-19 13:23:05 -0500

    cool changes

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings:
+    """A class to store all setting for alien Invasion."""
+
+    def __init__(self):
+        #screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
