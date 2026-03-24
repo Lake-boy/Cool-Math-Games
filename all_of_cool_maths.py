79ac4056456685cae8a996f1b184a38a9b65e73c
Lake <tgalvin@lakelandcollege.edu>
2026-03-24 13:57:24 -0500

    qgqg

--- all_of_cool_maths.py
+++ all_of_cool_maths.py
@@ -34,14 +34,26 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+                
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    #Move the ship to the right.
                     self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+                elif event.key == pygame.K_UP:
+                    self.ship.moving_up = True
+                elif event.key == pygame.K_DOWN:
+                    self.ship.moving_down = True
+
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
-                    #Stop moveing the ship to the right.
                     self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+                elif event.key == pygame.K_UP:
+                    self.ship.moving_up = False
+                elif event.key == pygame.K_DOWN:
+                    self.ship.moving_down = False
     
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
         """Initialize the ship and set its starting position."""
 
         self.screen = ai_game.screen
+        self.settings =ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         #load the ship image
@@ -15,13 +16,31 @@ class Ship:
         #start each new ship at the bottom of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        #store position floats.
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+
         #Movement Flags; starts with a ship that's not moving.
         self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
     
     def update(self):
         """Update the ship's position based on the movement flag."""
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
+        if self.moving_left:
+            self.x -= self.settings.ship_speed
+        if self.moving_down:
+            self.y += self.settings.ship_speed
+        if self.moving_up:
+            self.y -= self.settings.ship_speed
+
+        # Update rect objects
+        self.rect.x = self.x
+        self.rect.y = self.y
     
     def blitme(self):
         """Draw the ship at its current location."""
