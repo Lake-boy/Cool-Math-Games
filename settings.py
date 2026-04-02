5e4b72a79af8b39904d50b08f5ba1f974c02ab02
Lake <tgalvin@lakeladncollege.edu>
2026-04-02 14:09:38 -0500

    Yay

--- settings.py
+++ settings.py
@@ -5,7 +5,13 @@ class Settings:
         #screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (51, 51, 62)
+        self.bg_color = (230, 230, 230)
 
         #Ship settings
-        self.ship_speed = 2.5
+        self.ship_speed = 3.5
+
+        #Bullet settings
+        self.bullet_speed = 4.5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
