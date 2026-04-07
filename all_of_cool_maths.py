449d5d926f322b56c766721ea9f8608bccae0607
Lake <tgalvin@lakelandcollege.edu>
2026-04-07 13:55:59 -0500

    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHhh

--- all_of_cool_maths.py
+++ all_of_cool_maths.py
@@ -4,6 +4,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
@@ -22,6 +23,9 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
     
     def run_game(self):
         """Start the main loop for the game."""
@@ -82,16 +86,38 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+    def _create_fleet(self):
+        """Create the fleet of aliens."""
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+
+        curren_x, curren_y = alien_width, alien_height
+        while curren_y < (self.settings.screen_height - 3 * alien_height):
+            while curren_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(curren_x, curren_y)
+                curren_x += 2 * alien_width
+            
+            #Swaping rows 
+            curren_x = alien_width
+            curren_y += 2 * alien_height
+
+    def _create_alien(self, x_position, y_position):
+        """create an alien and place it in the row"""
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
+        self.aliens.add(new_alien)
+
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.ship.blitme()
+        self.aliens.draw(self.screen)
 
         pygame.display.flip()
-
-
 
 if __name__ == "__main__":
     #Makes a cool math game Instance and run the cool math game.
